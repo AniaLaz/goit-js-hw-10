281169949cb4438a052aa0083ca414b89f729d4b
AniaLaz <lazarenko.ania.sg@gmail.com>
2022-10-14 12:16:00 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './css/styles.css';
 import { fetchCountries } from "./fetchCountries";
+import debounce from "lodash.debounce"
+console.log(debounce);
 
 const countryListEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
@@ -8,29 +10,43 @@ let name = "";
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('input')
 
-inputEl.addEventListener("input", onInput)
+inputEl.addEventListener("input", debounce(onInput,DEBOUNCE_DELAY))
 
 function onInput(e) {
     name = inputEl.value
     console.log('name', name); 
     fetchCountries(name)
     .then(data => {
-        console.log("data", data)
+        console.log("data", data);
+        console.log('data.length', data.length);
+
+
         const marcup = createMarkup(data)
-        console.log("marcup", marcup); 
-        countryListEl.innerHTML = marcup
+        countryListEl.innerHTML = marcup;
+        const marcupInfo = createMarkupInfo(data)
+        countryInfoEl.innerHTML = marcupInfo
     })
     .catch(err => console.log(err))
     console.log("hello");
     }
 
 function  createMarkup(arr) {
+    console.log("arr.length", arr.length);
     return arr.map(country => `<li>
     <img src="${country.flags.svg}" alt="flags" class="flag" width = 20 >
     <h2>${country.name.common}</h2>
     </li>`).join('')
 }
 
-// function insertAdjacen(e) {
-//     countryListEl.insertAdjacentHTML('beforeend', createMarkup(arr)) 
-// }
+function createMarkupInfo(arr) {
+    return arr.map(country => `<li>
+    <img src="${country.flags.svg}" alt="flags" class="flag" width = 20 >
+    <h2>${country.name.common}</h2>
+    <ul>
+    <li>capital ${country.capital}</li>
+    <li>population ${country.population}</li>
+    <li>languages ${country.languages.value}</li>
+    </ul>
+    </li>
+    `).join('')
+}
